38deeab9f25c8cd0cb805cae817325af1af278e8
杨再润 <yangzair@outlook.com>
2022-12-21 22:11:19 +0800

    [实现]实时预警API

--- FlaskWeb/实时告警.py
+++ FlaskWeb/实时告警.py
@@ -68,31 +68,4 @@ def grc_alarm():
     # alarm = "HTTP/1.1 200 OK\rContent-Type: application/json\r" + alarm
 
 app.run(host="0.0.0.0", port=5000,debug=True)
-# r.expire("my-hash-key", 6)
-# print(r.hgetall("my-hash-key"))
-# print(r.hmget("my-hash-key", "松山湖南"))
-"""
-# 获取redis数据库连接
-r = redis.StrictRedis(host="node4", port=6379, db=0)
-# redis存入键值对
-r.set(name="key", value="value")
-# 读取键值对
-print(r.get("key"))
-# 删除
-print(r.delete("key"))
 
-# redis存入Hash值
-r.hset(name="name", key="key1", value="value1")
-r.hset(name="name", key="key2", value="value2")
-# 获取所有哈希表中的字段
-print(r.hgetall("name"))
-# 获取所有给定字段的值
-print(r.hmget("name", "key1", "key2"))
-# 获取存储在哈希表中指定字段的值。
-print(r.hmget("name", "key1"))
-# 删除一个或多个哈希表字段
-print(r.hdel("name", "key1"))
-# 过期时间
-r.expire("name", 60)  # 60秒后过期
-# 更多相关内容可以参考菜鸟教程
-"""
--- app.py
+++ app.py
@@ -3,32 +3,22 @@
 # @Time    : 2022/12/21 20:12
 # @Author  : 杨再润
 # @Site  :  https://tim-saijun.github.io/
-import redis
+
 import json
 import flask
 import datetime
 from flask_cors import CORS
+from util.source_redis import get_alarm_from_redis
+from util.FOOL import get_alarm_from_loc
 
 app = flask.Flask(__name__)
 CORS(app, resources=r'/*')	# 注册CORS, "/*" 允许访问所有api
-# r = redis.Redis(host='node4', port=6379, db=0, password='redis_password',\
-#                 decode_responses=True, charset='UTF-8', encoding='UTF-8')
-# r.hset("my-hash-key","1","23")
-def get_alarm_fromRedis():
-    r = redis.Redis(host='node4', port=6379, db=0, password='redis_password', decode_responses=True, charset='UTF-8', encoding='UTF-8')
-    alarm = r.hgetall("my-hash-key")
-    # r.expire("my-hash-key", 20)
-    if len(alarm) > 0:
-        # alarm = json.loads(alarm)
-        print(alarm)
-        return alarm
-    else:
-        return None
+
 
 @app.route('/alarm', methods=['GET'])
 def get_alarm():
     try:
-        alarm = get_alarm_fromRedis()
+        alarm = get_alarm_from_redis()
         # alarm加入时间戳
         alarm['time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         ret = {
@@ -52,7 +42,7 @@ def empty_alarm():
 
 @app.route('/alarm-grc', methods=['GET'])
 def grc_alarm():
-    alarm = get_alarm_fromRedis()
+    alarm = get_alarm_from_redis()
     # alarm加入时间戳
     alarm['时间'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     ret = {
@@ -66,10 +56,18 @@ def grc_alarm():
     else:
         return flask.jsonify({"status":0, "msg": "暂无告警信息！", "data": None})
 
-    # alarm = get_alarm_fromRedis()
-    # #将alarm转换为json格式,中文显示正常
-    # alarm = json.dumps(alarm, ensure_ascii=False)
-    # #将alarm转换为http响应格式
-    # alarm = "HTTP/1.1 200 OK\rContent-Type: application/json\r" + alarm
+@app.route('/right-bottom', methods=['GET'])
+def right_bottom():
+    return json.dumps(get_alarm_from_loc())
+
+@app.errorhandler(400)
+def crack_response(e):
+    hurl = flask.request.remote_addr     #请求来源的ip地址
+    hmsg =                     "Congratulates %s !\
+                                    You are an Administrator now,\
+                                    But you got the wrong token,\
+                                    the right token is 'FUCK YOU HACKER!' " % hurl
+    print("异常请求ip"+str(hurl))
+    return hmsg
 
 app.run(host="0.0.0.0", port=5000,debug=True)
